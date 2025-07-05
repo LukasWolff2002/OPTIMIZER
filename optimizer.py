478bb34ab854b0c963bffa22fc6b1ac4b4d6aad9
LukasWolff2002 <lwolff@miuandes.cl>
2025-07-05 11:40:37 -0400

    aumento de tiempo de calculo a 1 hora

--- optimizer.py
+++ optimizer.py
@@ -142,7 +142,7 @@ def optimize():
         search_parameters = pywrapcp.DefaultRoutingSearchParameters()
         search_parameters.first_solution_strategy = routing_enums_pb2.FirstSolutionStrategy.SAVINGS
         search_parameters.local_search_metaheuristic = routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH
-        search_parameters.time_limit.seconds = 30
+        search_parameters.time_limit.seconds = 3600#Una hora de limite de calculo
 
         solution = routing.SolveWithParameters(search_parameters)
 

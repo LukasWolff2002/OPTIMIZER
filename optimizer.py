a129786c9d952408de23fe9073fe28b7afe82e67
LukasWolff2002 <lwolff@miuandes.cl>
2025-07-07 14:24:04 -0400

    modifico puerto a 5000

--- optimizer.py
+++ optimizer.py
@@ -276,4 +276,4 @@ def optimize():
         return jsonify(error=f"Error interno: {str(e)}"), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)

7f7a6c8243447887f6d6fb73eee0fd2be3b1c734
LukasWolff2002 <lwolff@miuandes.cl>
2025-07-07 16:02:28 -0400

    vuelvo un commit

--- optimizer.py
+++ optimizer.py
@@ -171,9 +171,6 @@ def optimize():
         search_parameters = pywrapcp.DefaultRoutingSearchParameters()
         search_parameters.first_solution_strategy = routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
         search_parameters.local_search_metaheuristic = routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH
-
-        print("========== Comienzo a Calcular ==========")
-
         search_parameters.time_limit.seconds = 30
 
         solution = routing.SolveWithParameters(search_parameters)

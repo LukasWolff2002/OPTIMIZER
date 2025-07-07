a97d0e945e6aa3009f290dfc7b748a3bcf6b13f3
LukasWolff2002 <lwolff@miuandes.cl>
2025-07-07 16:06:41 -0400

    bajo el tiempo

--- optimizer.py
+++ optimizer.py
@@ -171,7 +171,7 @@ def optimize():
         search_parameters = pywrapcp.DefaultRoutingSearchParameters()
         search_parameters.first_solution_strategy = routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
         search_parameters.local_search_metaheuristic = routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH
-        search_parameters.time_limit.seconds = 30
+        search_parameters.time_limit.seconds = 5
 
         solution = routing.SolveWithParameters(search_parameters)
 

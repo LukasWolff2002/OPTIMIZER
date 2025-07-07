1e0f6e9c4a623eff4462fd94e6a639d960753f9c
LukasWolff2002 <lwolff@miuandes.cl>
2025-07-07 14:47:01 -0400

    cambio puerto

--- optimizer.py
+++ optimizer.py
@@ -282,4 +282,4 @@ def optimize():
         return jsonify(error=f"Error interno: {str(e)}"), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=8080, debug=False)
